44fcdab1876a03dab28aac51e32140837830cb93
VaishnavChaudhari <65059084+VaishnavChaudhari@users.noreply.github.com>
2020-09-05 22:32:33 +0530

    2nd Commit

--- app/src/main/java/ucompare/in/unquotedcodeacademy/MainActivity.java
+++ app/src/main/java/ucompare/in/unquotedcodeacademy/MainActivity.java
@@ -24,16 +24,37 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Question> questions;
 
     // TODO 3-A: Declare View member variables
+    ImageView questionImageView;
+           TextView questionTextView;
+           TextView questionsRemainingTextView;
+            Button answer0Button;
+    Button answer1Button;
+    Button answer2Button;
+    Button answer3Button;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // TODO 2-G: Show app icon in ActionBar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_unquote_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setElevation(0);
 
         setContentView(R.layout.activity_main);
 
         // TODO 3-B: assign View member variables
+        questionImageView =findViewById(R.id.iv_main_question_image);
+        questionsRemainingTextView = findViewById(R.id.tv_main_questions_remaining_count);
+        questionTextView = findViewById(R.id.tv_main_question_title);
+        answer0Button = findViewById(R.id.btn_main_answer_0);
+        answer1Button = findViewById(R.id.btn_main_answer_1);
+        answer2Button = findViewById(R.id.btn_main_answer_2);
+        answer3Button = findViewById(R.id.btn_main_answer_3);
+        submitButton = findViewById(R.id.btn_main_submit_answer);
+
 
         // TODO 4-E: set onClickListener for each answer Button
 
@@ -75,13 +96,61 @@ public class MainActivity extends AppCompatActivity {
         questions = new ArrayList<>();
 
         // TODO 2-H: Provide actual drawables for each of these questions!
-        Question question0 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question1 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question2 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question3 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question4 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-        Question question5 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
-
+        Question question0 = new Question(R.drawable.img_quote_0,
+                "Pretty good advice,\n" +
+                        "and perhaps a scientist\n" +
+                        "did say it… Who\n" +
+                        "actually did?",
+                "Albert Einstein ",
+                "Isaac Newton",
+                "Rita Mae Brown",
+                "Rosalind Franklin", 2);
+        Question question1 = new Question(R.drawable.img_quote_1,
+                "Was honest Abe\n" +
+                        "honestly quoted? Who\n" +
+                        "authored this pithy bit\n" +
+                        "of wisdom?",
+                "Edward Stieglitz",
+                "Maya Angelou",
+                "TAbraham Lincoln",
+                "Ralph Waldo Emerson",
+                0);
+        Question question2 = new Question(R.drawable.img_quote_2,
+                "This is a test question!",
+                "Test Answer 0",
+                "Test Answer 1",
+                "Test Answer 2",
+                "Test Answer 3",
+                2);
+        Question question3 = new Question(R.drawable.img_quote_3,
+                "Easy advice to read,\n" +
+                        "difficult advice to\n" +
+                        "follow — who actually said it?",
+                "Martin Luther King Jr",
+                "Mother Teresa",
+                "Fred Rogers",
+                "Oprah Winfrey",3
+        );
+        Question question4 = new Question(R.drawable.img_quote_4,
+                "Insanely inspiring,\n" +
+                        "insanely incorrect\n" +
+                        "(maybe). Who is the\n" +
+                        "true source of this\n" +
+                        "inspiration?\n",
+                "Nelson Mandela ",
+                "Harriet Tubman",
+                "Mahatma Gandhi",
+                "Nicholas Klein",
+                2);
+        Question question5 = new Question(R.drawable.img_quote_5,
+                "A peace worth striving\n" +
+                        "for — who actually\n" +
+                        "reminded us of this?",
+                "Malala Yousafzai ",
+                "Martin Luther King Jr. ",
+                "Liu Xiaobo"
+                , "Dalai Lama",
+                1);
         questions.add(question0);
         questions.add(question1);
         questions.add(question2);
